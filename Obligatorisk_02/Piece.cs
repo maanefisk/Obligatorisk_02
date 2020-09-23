890abc60a27386897ed5262668cf96fb33aab8f1
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-09-23 12:40:54 +0200

    Initial commit

---
+++ Obligatorisk_02/Piece.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obligatorisk_02
+{
+    class Piece
+    {
+        public string Symbol { get; }
+        private readonly string _type;
+
+        public Piece(string type, string symbol)
+        {
+            _type = type;
+            Symbol = symbol;
+        }
+
+        public bool Move(string fromPosition, string toPosition)
+        {
+            if (_type == "Rook") return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+            if (_type == "Bishop")
+            {
+                var diffCol = fromPosition[0] - toPosition[0];
+                var diffRow = fromPosition[1] - toPosition[1];
+                return Math.Abs(diffRow) == Math.Abs(diffCol);
+            }
+            return true;
+        }
+    }
+}
